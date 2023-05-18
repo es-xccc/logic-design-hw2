e67f6aaca2805c8a6efba4a97562b47f5a2c15fc
r2605572 <r2605572@gmail.com>
2023-05-19 01:04:16 +0800

    wtf

--- hw2_2/hw2_2.srcs/sources_1/imports/hw2_2/hw2_2.v
+++ hw2_2/hw2_2.srcs/sources_1/imports/hw2_2/hw2_2.v
@@ -5,6 +5,111 @@ input clk, reset;
 input[7:0] data_in;
 input[8:0] c;
 output[7:0] data_out;
+reg r0[7:0],r1[7:0],r2[7:0],r_out[7:0],i,dest[7:0];
+always@(negedge reset)begin
+for(i=0;i<8;i=i+1)begin
+r0[i]<=0;
+r1[i]<=0;
+r2[i]<=0;
+r_out[i]<=0;
+end
+end
+always@(posedge clk)begin
+end
+endmodule
 
 
+module mux(c1,c2,r0,r1,r2,din,src);
+input c1,c2,r0,r1,r2,din;
+output reg src;
+always@(c1 || c2)begin
+if(c1==0)begin
+    if(c2==0)begin
+        assign src=r0;
+    end
+    else begin
+        assign src=r1;
+    end
+end
+else begin
+    if(c2==0)begin
+        assign src=r0;
+    end
+    else begin
+        assign src=din;
+    end
+end
+end
+endmodule
+
+
+module demux(dest,c1,c2,r0,r1,r2,r_out);
+input dest,c1,c2;
+output reg r0,r1,r2,r_out;
+always@(c1 || c2)begin
+assign r0=0;
+assign r1=0;
+assign r2=0;
+assign r_out=0;
+if(c1==0)begin
+    if(c2==0)begin
+        assign r0=dest;
+    end
+    else begin
+        assign r1=dest;
+    end
+end
+else begin
+    if(c2==0)begin
+        assign r2=dest;
+    end
+    else begin
+        assign r_out=dest;
+    end
+end
+end
+endmodule
+
+
+module alu(src_a,src_b,c0,c1,c2,dest);
+input src_a,src_b,c0,c1,c2;
+output reg dest;
+always@(c0||c1||c2)begin
+if(c0==0)begin
+    if(c1==0)begin
+        if(c2==0)begin
+            assign dest=src_a;
+        end
+        else begin
+            assign dest=src_a+src_b;
+        end
+    end
+    else begin
+        if(c2==0)begin
+            assign dest=src_a-src_b;
+        end
+        else begin
+            assign dest=src_a&src_b;
+        end
+    end
+end
+else begin
+    if(c1==0)begin
+        if(c2==0)begin
+            assign dest=src_a|src_b;
+        end
+        else begin
+            assign dest=src_a^src_b;
+        end
+    end
+    else begin
+        if(c2==0)begin
+            assign dest=0;
+        end
+        else begin
+            assign dest=0;
+        end
+    end
+end
+end
 endmodule
