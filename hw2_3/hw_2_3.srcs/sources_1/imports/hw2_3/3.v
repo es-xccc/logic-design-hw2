f57fe99e3f12a2af2b60dda7151ccf5e7a858ebc
r2605572 <r2605572@gmail.com>
2023-05-30 14:20:03 +0800

    3 still fixing

--- hw2_3/hw_2_3.srcs/sources_1/imports/hw2_3/3.v
+++ hw2_3/hw_2_3.srcs/sources_1/imports/hw2_3/3.v
@@ -8,52 +8,12 @@ module hw2_3(
     output reg walk_right,
     output reg aaah );
     reg [1:0]state,next;
-    
-always @(posedge areset)begin
-    aaah<=0;
-    walk_left<=1;
-    walk_right<=0;
-end
 
 always @(areset, turn_left, turn_right)begin
-    state[1]=walk_left;
-    state[0]=walk_right;
+     
 end
 
 always @(posedge clk)begin
-    if(areset==0)begin
-        if(ground==0)begin
-            aaah<=1;
-            walk_left<=0;
-            walk_right<=0;
-        end
-        else if(turn_left==1 && turn_right==1)begin
-            if(walk_left==1)begin
-                aaah<=0;
-                walk_left<=0;
-                walk_right<=1;
-            end
-            else if(walk_right==1)begin
-                aaah<=0;
-                walk_left<=1;
-                walk_right<=0;
-            end
-        end
-        else if(turn_left==1)begin
-            aaah<=0;
-            walk_left<=1;
-            walk_right<=0;
-        end
-        else if(turn_right==1)begin
-            aaah<=0;
-            walk_left<=0;
-            walk_right<=1;
-        end
-        else begin
-            aaah<=0;
-            walk_left<=state[1];
-            walk_right<=state[0];
-        end
-    end
+    
 end
 endmodule
