e4bf9055c9860e82b1ab8bf5f7bafa4d9e7bbf67
r2605572 <r2605572@gmail.com>
2023-05-30 21:47:45 +0800

    finish 3

--- hw2_3/hw_2_3.srcs/sources_1/imports/hw2_3/3.v
+++ hw2_3/hw_2_3.srcs/sources_1/imports/hw2_3/3.v
@@ -8,12 +8,46 @@ module hw2_3(
     output reg walk_right,
     output reg aaah );
     reg [1:0]state,next;
-
-always @(turn_left, turn_right)begin
-     
-end
+    reg [1:0]temp;
 
 always @(posedge clk, posedge areset)begin
-    
+    if(areset==1)begin
+        walk_left<=1;
+        walk_right<=0;
+        aaah=0;
+        temp[1]<=walk_left;
+        temp[0]<=walk_right;
+    end
+    else if(ground==0)begin
+        walk_left<=0;
+        walk_right<=0;
+        aaah=1;
+    end
+    else if(turn_left==1 && turn_right==1)begin
+        temp[1]<=walk_right;
+        temp[0]<=walk_left;
+        walk_left<=walk_right;
+        walk_right<=walk_left;
+        aaah<=0;
+    end
+    else if(turn_left==1)begin
+        walk_left<=1;
+        walk_right<=0;
+        aaah<=0;
+        temp[1]<=walk_left;
+        temp[0]<=walk_right;
+    end
+    else if(turn_right==1)begin
+        walk_left<=0;
+        walk_right<=1;
+        aaah<=0;
+        temp[1]<=walk_left;
+        temp[0]<=walk_right;
+    end
+    else begin
+        walk_left<=temp[1];
+        walk_right<=temp[0];
+        aaah<=0;
+    end
 end
 endmodule
